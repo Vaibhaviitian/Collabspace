ba488db38f962d661d881568fadf9e253b3a7e73
Vaibhaviitian <aryanvaibhav74@gmail.com>
2025-05-02 09:33:50 +0530

    adding

--- Frontend/src/Elements/Notifications.jsx
+++ Frontend/src/Elements/Notifications.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { motion } from "framer-motion";
+import { motion ,AnimatePresence} from "framer-motion";
 
 export default function NotificationTabs() {
   const [activeTab, setActiveTab] = useState("sent");
